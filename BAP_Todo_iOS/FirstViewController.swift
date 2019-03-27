e144fec87316925b61b0cd115a9f75a9b536fe4a
Jonas De Ridder <jonasderidder@ptr-ggckpwq2aijbw1ns64x.18120a2.ip6.access.telenet.be>
2019-03-27 12:35:25 +0100

    Initial Commit

---
+++ BAP_Todo_iOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BAP_Todo_iOS
+//
+//  Created by Jonas De Ridder on 27/03/2019.
+//  Copyright © 2019 Jonas De Ridder. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BAP_Todo_iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BAP_Todo_iOS
+//
+//  Created by Jonas De Ridder on 27/03/2019.
+//  Copyright © 2019 Jonas De Ridder. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
